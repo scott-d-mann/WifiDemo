174cb1506196c0e31d667cd368f17ebaaa8f4e57
Marita Fitzgerald <maritafitzgerald@gmail.com>
2020-08-21 12:43:00 +1000

    changes

--- app/src/main/java/com/example/wifidemo/MainActivity.java
+++ app/src/main/java/com/example/wifidemo/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.wifidemo;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button updateBtn;
+    Button updateBtn, scanBtn;
     TextView is5Gtv, isEnabledtv, getWifiTv;
     String TAG = "WifiDemo";
     private WifiManager wifiManager;
+    Spinner spinner;
+    BroadcastReceiver wifiScanReceiver;
 
 
     @Override
@@ -24,12 +33,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         updateBtn = (Button) findViewById(R.id.updateBtn);
+        scanBtn = (Button) findViewById(R.id.scanBtn);
         is5Gtv = (TextView) findViewById(R.id.is5GTV);
         isEnabledtv = (TextView) findViewById(R.id.isenabledTV);
         getWifiTv = (TextView) findViewById(R.id.getWifiTV);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
         //create wifi manager object
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        //intent filter & register broadcast receiver
+        wifiScanReceiver = new MyWifiReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        this.registerReceiver(wifiScanReceiver, intentFilter);
+
+        //check permissions
+        checkPermissions();
 
         //register listeners
         updateBtn.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +60,46 @@ public class MainActivity extends AppCompatActivity {
                 getWifiState();
             }
         });
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(wifiScanReceiver);
+        super.onDestroy();
+    }
+
+    private void checkPermissions()
+    {
+
+    }
+
+    private void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+        Log.d(TAG, results.toString());
+    }
+
+    private void scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        Log.d(TAG, "Scan failed");
+    }
+
+    public void startScan()
+    {
+        boolean success = wifiManager.startScan();
+        Log.d(TAG, "Scan started");
+        if (!success) {
+            // scan failure handling
+            scanFailure();
+            Log.d(TAG, "Scan fail in start");
+        }
     }
 
     public void is5Gsupported() {
@@ -81,4 +141,21 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.d(TAG, "getWifi Complete");
     }
+
+    public class MyWifiReceiver extends BroadcastReceiver {
+        private static final String TAG = "MyWifiReceiver";
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            boolean success = intent.getBooleanExtra(
+                    WifiManager.EXTRA_RESULTS_UPDATED, false);
+            if (success) {
+                Log.d(TAG, "Scan success in rx");
+                scanSuccess();
+            } else {
+                // scan failure handling
+                scanFailure();
+                Log.d(TAG, "Scan fail in rx");
+            }
+        }
+    };
 }
